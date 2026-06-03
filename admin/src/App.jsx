85e69b8c58d6884efebc1c05775631bfe9d6cf92
Badal Kumar <badalkumar0159@gmail.com>
2026-06-03 09:14:41 +0530

    Added routes and  admin login page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Sidebar from './components/sidebar'
+import Orders from './pages/Orders'
+import Add from './pages/Add'
+import List from './pages/List'
+import Login from './components/Login'
 
 const App = () => {
+
+  const [token, setToken] = useState('');
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-        <Navbar />
-        <hr className='text-gray-300' />
-        <div className='flex w-full'>
-          <Sidebar />
-        </div>
-      </>
+
+      {token === "" ? <Login /> :               //if not authenticated display login page
+        <>                                      //else display admin panel
+          <Navbar />
+          <hr className='text-gray-300' />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base bg-pink-100'>
+              <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
     </div>
   )
 }
